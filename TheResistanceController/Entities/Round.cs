d12cd73a9d9d761b12b72bbe946ab2dedb071e08
Vitor Pozzebon <vitor.scalari@gmail.com>
2024-11-05 07:38:03 -0300

    subindo o projeto

---
+++ TheResistanceController/Entities/Round.cs
@@ -0,0 +1,108 @@
+﻿using System;
+
+namespace TheResistanceController.Entities
+{
+    internal class Round
+    {
+        public int Number { get; set; }
+        public string Leader { get; set; }
+        public List<string> Crew { get; set; } = new List<string>();
+        public int Approvals { get; set; }
+        public int Reproaches { get; set; }
+        public int successVotes { get; set; }
+        public int failureVotes { get; set; }
+
+        public Round(int number, string leader, int approvals, int reproaches, int successVotes, int failureVotes)
+        {
+            Number = number;
+            Leader = leader;
+            Approvals = approvals;
+            Reproaches = reproaches;
+            this.successVotes = successVotes;
+            this.failureVotes = failureVotes;
+        }
+
+        public Round() { }
+
+        public Round createRound()
+        {
+            Console.Clear();
+            Round newRound = new Round();
+            
+            Console.Write("Digite o número da rodada: ");
+            newRound.Number = int.Parse(Console.ReadLine());
+            Console.Write("Digite o nome do líder da rodada: ");
+            newRound.Leader = (Console.ReadLine());
+            Console.Write("Digite o número de escolhidos: ");
+            int crewNumber = int.Parse(Console.ReadLine());
+            for (int i = 0; i < crewNumber; i++)
+            {
+                Console.Write($"Digite o nome do escolhido {i + 1}: ");
+                string name = Console.ReadLine();
+                newRound.Crew.Add(name);
+            }
+            Console.Write("Digite o número de votos aprovados: ");
+            newRound.Approvals = int.Parse(Console.ReadLine());
+            Console.Write("Digite o número de votos reprovados: ");
+            newRound.Reproaches = int.Parse(Console.ReadLine());
+            if (newRound.Approvals > newRound.Reproaches)
+            {
+                Console.Write("Digite o número de sucessos: ");
+                newRound.successVotes = int.Parse(Console.ReadLine());
+                Console.Write("Digite o número de fracassos: ");
+                newRound.failureVotes = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("---------------------------------------------");
+            Console.WriteLine("RODADA INSERIDA COM SUCESSO!!!");
+            Thread.Sleep(1000);
+            return newRound;
+        }
+
+        public void seeRounds(List<Round> rounds)
+        {
+            Console.Clear();
+            if(rounds.Count == 0)
+            {
+                Console.WriteLine("Nenhuma rodada cadastrada no momento.");
+            }
+            foreach (Round r in rounds)
+            {
+                Console.WriteLine(r.ToString());
+            }
+            Console.WriteLine("\nAperte Enter para voltar.");
+            Console.ReadLine();
+        }
+
+        public override string ToString()
+        {
+            string crewFormated = string.Join(", ", Crew);
+            if (Approvals > Reproaches)
+            {
+                return "-----------------------------------" +
+                $"\nNÚMERO DA RODADA: {Number}" +
+                $"\nLÍDER DA RODADA: {Leader}" +
+                $"\nESCOLHIDOS: {crewFormated}" +
+                $"\nVOTOS APROVADOS: {Approvals}" +
+                $"\nVOTOS REPROVADOS: {Reproaches}" +
+                $"\nVOTOS DE SUCESSO: {successVotes}" +
+                $"\nVOTOS DE FRACASSO: {failureVotes}" +
+                "\n---------------------------------------" +
+                "\nMISSÃO CONCLUÍDA E APROVADA!!!" +
+                "\n---------------------------------------";
+            }
+            else
+            {
+                return "\n-----------------------------------" +
+                $"\nNÚMERO DA RODADA: {Number}" +
+                $"\nLÍDER DA RODADA: {Leader}" +
+                $"\nESCOLHIDOS: {crewFormated}" +
+                $"\nVOTOS APROVADOS: {Approvals}" +
+                $"\nVOTOS REPROVADOS: {Reproaches}" +
+                "\n-----------------------------------------------" +
+                "\nMISSÃO NÃO INICIADA (REPROVADA)!!!" +
+                "\n-----------------------------------------------";
+            }
+        }
+    }
+}
---
+++ TheResistanceController/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using TheResistanceController.Entities;
+
+namespace TheResistanceController
+{
+    class Program
+    {
+        public static void seePlayers(List<string> p)
+        {
+            Console.Clear();
+
+            Console.WriteLine("-------------------------------------------------");
+            Console.WriteLine("JOGADORES ATIVOS: ");
+            foreach (string player in p)
+            {
+                Console.WriteLine(player);
+            }
+            Console.WriteLine("-------------------------------------------------");
+
+            Console.WriteLine("Pressione Enter para voltar...");
+            Console.ReadLine();
+        }
+
+        static void Main(string[] args)
+        {
+            // Lista de jogadores
+            List<string> players = new List<string>();
+
+            // Lista de rouds
+            List<Round> rounds = new List<Round>();
+
+            // Número de jogadores
+            Console.Write("Digite o número de jogadores: ");
+            int numPlayers = int.Parse(Console.ReadLine());
+
+            if (numPlayers <= 0 || numPlayers > 10)
+            {
+                Console.WriteLine("\n----------------------------------------------------------------------");
+                while (numPlayers <= 0 || numPlayers > 10)
+                {
+                    Console.Write("Número de jogadores inválido. Digite outro número: ");
+                    numPlayers = int.Parse(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine("\n----------------------------------------------------------------------");
+
+            // Coletando os nomes dos jogadores
+            for (int i = 0; i < numPlayers; i++)
+            {
+                Console.Write($"\nDigite o nome do jogador {i + 1}: ");
+                string name = Console.ReadLine();
+                players.Add(name);
+                Console.WriteLine("JOGADOR ADICIONADO COM SUCESSO!!!");
+            }
+
+            Console.WriteLine("\n----------------------------------------------------------------------");
+            Console.WriteLine("\nJOGADORES ATIVOS:");
+            foreach (string name in players)
+            {
+                Console.WriteLine(name);
+            }
+            Console.WriteLine("\n----------------------------------------------------------------------");
+
+            // Limpando o console
+            Thread.Sleep(3000);
+
+            // Criando e usando o menu de opções
+            Round round = new Round();      // Instanciamento da classe Round
+            int op = -1;
+
+            while (op != 0)
+            {
+                Console.Clear();
+                Console.WriteLine("-------------------------------------------------");
+                Console.WriteLine("MENU DE OPÇÕES - THE RESISTANCE");
+                Console.WriteLine("-------------------------------------------------");
+                Console.WriteLine("ESCOLHA UMA DAS OPÇÕES ABAIXO: ");
+                Console.WriteLine("1. VER RODADAS ANTERIORES");
+                Console.WriteLine("2. VER JOGADORES ATIVOS");
+                Console.WriteLine("3. CADASTRAR UMA NOVA RODADA");
+                Console.WriteLine("0. SAIR DO CONTROLADOR DE THE RESISTANCE");
+                Console.Write("\nOPÇÃO: ");
+                op = int.Parse(Console.ReadLine());
+
+                switch (op)
+                {
+                    case 1: 
+                        round.seeRounds(rounds);
+                        break;
+
+                    case 2:
+                        seePlayers(players);
+                        break;
+
+                    case 3: 
+                        Round newRound = round.createRound();
+                        rounds.Add(newRound);
+                        break;
+
+                    case 0:
+                        Console.Clear();
+                        Console.WriteLine("SAINDO DO CONTROLADOR...");
+                        Thread.Sleep(1000);
+                        break;
+
+                    default:
+                        Console.WriteLine("-------------------------------------------------");
+                        Console.Write("OPÇÃO INVÁLIDA. DIGITE UMA NOVA OPÇÃO: ");
+                        op = int.Parse(Console.ReadLine());
+                        break;
+                }
+            }
+        }
+    }
+}
